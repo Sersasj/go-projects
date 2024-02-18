69a56db35a4ab713dbfc750e22fe1177a29f4766
Sergio A. <sasjsergioalvarezjunior@gmail.com>
2024-02-18 19:43:45 -0300

    feat: testes, fim aula 3

--- gin-rest-api/main_test.go
+++ gin-rest-api/main_test.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"go-gin-api/controllers"
+	"go-gin-api/database"
+	"go-gin-api/models"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 )
 
+var ID int
+
 func setupRouter() *gin.Engine {
+	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 	// r.GET("/ping", ping)
 	// r.GET("/alunos", getAlunos)
@@ -27,10 +34,43 @@ func TestVerifyPing(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/ping", nil)
 	resp := httptest.NewRecorder()
 	r.ServeHTTP(resp, req)
-	if resp.Code != 200 {
-		t.Errorf("Expected 200 but got %d", resp.Code)
-	}
-	if resp.Body.String() != "{\"message\":\"pong\"}" {
-		t.Errorf("Expected {\"message\":\"pong\"} but got %s", resp.Body.String())
-	}
+	assert.Equal(t, http.StatusOK, resp.Code, "OK response is expected")
+	respMock := `{"message":"pong"}`
+	respBody, _ := io.ReadAll(resp.Body)
+	assert.Equal(t, respMock, string(respBody), "Expected pong")
+}
+
+func createAlunoMock() {
+	aluno := models.Aluno{Nome: "Teste", Idade: 20, Email: "teste@mail.com"}
+	database.DB.Create(&aluno)
+	ID = int(aluno.ID)
+}
+
+func deleteAlunoMock() {
+	var aluno models.Aluno
+	database.DB.Delete(&aluno, ID)
+}
+func TestVerifyGetAlunos(t *testing.T) {
+	database.Connect()
+	createAlunoMock()
+	defer deleteAlunoMock()
+	r := setupRouter()
+	r.GET("/alunos", controllers.GetAlunos)
+	req, _ := http.NewRequest("GET", "/alunos", nil)
+	resp := httptest.NewRecorder()
+	r.ServeHTTP(resp, req)
+	assert.Equal(t, http.StatusOK, resp.Code, "OK response is expected")
+
+}
+
+func TestVerifyGetAlunoByEmail(t *testing.T) {
+	database.Connect()
+	createAlunoMock()
+	defer deleteAlunoMock()
+	r := setupRouter()
+	r.GET("/alunos/email/:email", controllers.GetAlunosByEmail)
+	req, _ := http.NewRequest("GET", "/alunos/email/teste@mail.com", nil)
+	resp := httptest.NewRecorder()
+	r.ServeHTTP(resp, req)
+	assert.Equal(t, http.StatusOK, resp.Code, "OK response is expected")
 }
